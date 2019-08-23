2126fb3d949ac4edead1a3ca7d91e26e8c3cee06
Oscar Malo <omalo@ford.com>
2019-08-23 13:00:16 -0500

    Update TDD

---
+++ spec/guiones_spec.rb
@@ -0,0 +1,24 @@
+require './lib/generarPalabra.rb'
+describe Generar do
+	it 'Si recibo dog debo ver ---' do
+		#Arrange (preparacion de escenario de pruebas)
+        generar = Generar.new
+		#Act
+		result = generar.guiones("dog")  
+		#Assert
+		expect(result).to eq "---"
+    end
+	it 'Si recibo paz debo ver ---' do
+		#Arrange (preparacion de escenario de pruebas)
+        generar = Generar.new
+		#Act
+		result = generar.guiones("paz")  
+		#Assert
+		expect(result).to eq "---"
+	end    
+end
+
+
+
+#Si recibo paz debo ver '---'
+#Si recibo costumbre debo ver '---------'
